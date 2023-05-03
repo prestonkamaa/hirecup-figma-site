f1f86500ee5d3581d444f9d215dfe616c68f6619
prestonkamaa <prestonkamaa@gmail.com>
2023-05-03 08:53:03 +0300

    Cleanup

--- components/ApplyNow.js
+++ components/ApplyNow.js
@@ -5,12 +5,11 @@ export default function ApplyNow() {
   return (
     <section className=" container mx-auto px-6">
       
-      <div className=" grid lg:grid-cols-2">
-        {/* <div className=" px-6 mx-auto bg-[url('/images/applynoow.png')]">  */}
+      <div className=" grid lg:grid-cols-2"> 
 
         <div className=" px-6 mx-auto bg-[url('/images/applynoow.png')] bg-no-repeat bg-center bg-contain">
             <div className=" mt-4"><Image src="/images/people1.png" width="446" height="520" alt="" /> 
-            {/* <Image src="/images/applynoow.png" width="520" height="464" alt="" />  */}</div>
+        </div>
         </div> 
         <div className=" py-20  grid px-6 ">
             <h1 className="text-[#F3C623] text-lg font-semibold pb-0">
@@ -39,24 +38,7 @@ export default function ApplyNow() {
                 looking for an employee?
               </p>
             </div>
-          </div>
-
-          {/* <div className="flex gap-10 pt-8 items-center"> 
-            <button type="button" className="space-x-2 items-center text-white bg-[#127681] hover:bg-[#0e5a63] focus:outline-none focus:ring-4 focus:ring-blue-300 font-bold rounded-full text-lg px-10 py-2.5 text-center  mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 inline-flex">
-                <MdOutlineFileUpload size="1.5rem"/> <span>Upload CV</span>
-            </button>
-            
-            <p className="text-[#127681] text-lg font-bold underline">
-              looking for an employee?
-            </p>
-          </div> */}
-
-
-  
-
-
-
-
+          </div>   
         </div>
       </div>
 
